255cbbc9dc0c5bb55c42e40e581bb61954d22086
ahnafhafidz <80145137+ahnafhafidz@users.noreply.github.com>
2021-03-12 09:33:02 +0700

    Add files via upload

---
+++ I0320060_exercise2.1.py
@@ -0,0 +1,18 @@
+#tipe data boolean
+print(true)
+
+#tipe data string
+print("Ayo belajar Phyton")
+print('Belajar Phyto Sangat Mudah')
+
+#tipe data integer
+print(20)
+
+#tipe data float
+print(3.14)
+
+#tipe data Hexadecimal
+print(9a)
+
+#tipe data Complex
+print(5j)
